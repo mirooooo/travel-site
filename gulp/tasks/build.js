7de8a805c9acced1c97ebf9cc8e97f94f565b5be
Miro <mr.mladenoff@gmail.com>
2018-11-21 11:24:52 -0800

    Final touches before going live

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,13 +1,39 @@
+// build.js is used for preparing our files go live.... Creating 'dist' folder and storing all minified uglyfied files there
+
+
+
 var gulp = require("gulp");
+// imagemin- minification images
 var imagemin = require('gulp-imagemin');
+// del - delete a folder
 var del = require('del');
+// usemin - Bundeling css and js files----- have to rap up in sandwich your css and js files in index.html 
 var usemin = require('gulp-usemin');
+// cssnano - minification css file
+var cssnano = require('gulp-cssnano');
+// ===========================
+var rev = require('gulp-rev');
+// for minification-compresing javascript
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('previewDist', function () {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "dist"
+        }
+    });
+});
+
+
 
 gulp.task('deleteDistFolder', function () {
     return del('./dist');
 });
 
-gulp.task("optimizeImages", ['deleteDistFolder'], function () {
+gulp.task("optimizeImages", ['deleteDistFolder', 'icons'], function () {
     return gulp.src(["./app/assets/images/**/*", "!./app/assets/images/icons", "!./app/assets/images/icons/**/*"])
         .pipe(imagemin({
             progressive: true,
@@ -17,9 +43,20 @@ gulp.task("optimizeImages", ['deleteDistFolder'], function () {
         .pipe(gulp.dest("./dist/assets/images"));
 });
 
-gulp.task('usemin', ['deleteDistFolder'], function () {
+gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function () {
     return gulp.src('./app/index.html')
-        .pipe(usemin())
+        .pipe(usemin({
+            css: [function () {
+                return rev()
+            }, function () {
+                return cssnano()
+            }],
+            js: [function () {
+                return rev()
+            }, function () {
+                return uglify()
+            }]
+        }))
         .pipe(gulp.dest('./dist'));
 });
 
